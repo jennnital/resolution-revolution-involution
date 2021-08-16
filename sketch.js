05b0ed2aa52208d1935f0ab47f3796d3f9ffbf9c
jemm <52009482+jennnital@users.noreply.github.com>
2021-08-16 16:07:49 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,16 +9,29 @@ function setup() {
 	imageMode(CENTER);
 
 	glitch = new Glitch();
-	loadImage('fish.png', function(im){
+	loadImage('keywords.png', function(im){
 		glitch.loadImage(im);
 	});
 }
 
-function draw() {
+var minByte=100;
+var maxByte=104;
+
+function draw(){
+
 	glitch.resetBytes();
 
-	glitch.replaceBytes(100, 104); // swap all decimal byte 100 for 104
+	glitch.replaceBytes(minByte, maxByte); // swap all decimal byte 100 for 104
 	glitch.randomBytes(1); // add one random byte for movement
+
+	glitch.buildImage();
+	image(glitch.image, width/2, height/2)
+
+}
+function mousePressed(){
+	glitch.resetBytes();
+	glitch.replaceBytes(minByte*randomByte, maxByte*randomByte); // swap all decimal byte 100 for 104
+	glitch.randomBytes(4); // add one random byte for movement
 
 	glitch.buildImage();
 	image(glitch.image, width/2, height/2)
