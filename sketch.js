e20ac2eef9dd93f47cdd92fa7f8d7d095c692baf
jemm <52009482+jennnital@users.noreply.github.com>
2021-08-16 17:24:25 +0100

    added some text

--- sketch.js
+++ sketch.js
@@ -18,11 +18,13 @@ var minByte=100;
 var maxByte=104;
 var byteMultiplier=0.2;
 var pixelateMultiplier=1;
+var mouseclickedcount=0;
 
 function draw(){
 	// glitch.resetBytes();
 	background(0);
 	if(mouseIsPressed){
+		mouseclickedcount+=1;
 		pixelateMultiplier-=0.05;
 		glitch.replaceBytes(minByte*(byteMultiplier), maxByte*(byteMultiplier)); // swap all decimal byte 100 for 104
 		glitch.randomBytes(1); // add one random byte for movement
@@ -32,4 +34,10 @@ function draw(){
 	glitch.buildImage();
 	image(glitch.image, width/2, height/2)
 
+	if(mouseclickedcount>3){
+		text('Generation loss occurs when the transduction of data creates artifacts, thus, these progressive,',width/2, height/2);
+		text('overlapping processes compound themselves into a spiral of self-destruction.,',width/2, height/2+10);
+		textAlign(CENTER, CENTER);
+
+	}
 }
